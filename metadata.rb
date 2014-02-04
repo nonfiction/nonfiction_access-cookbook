0e1e5228cd212fea4025ea610d65d591d96cd159
Darron Froese <darron@froese.org>
2014-02-04 14:17:57 -0700

    First commit.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+name             'nonfiction_access'
+maintainer       'Darron Froese'
+maintainer_email 'darron@froese.org'
+license          'Apache 2.0'
+description      'Installs/configures something'
+version          '0.1.0'
+recipe           'nonfiction_access::default', 'Installs/configures something'
